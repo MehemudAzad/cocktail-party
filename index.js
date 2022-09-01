9c24dff935fe5885adac9045ddefdcd62909088d
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-09-01 21:37:21 +0600

    added

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-function loadCocktails(search){
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${search}`
-    fetch(url)
-    .then(res=>res.json())
-    .then(data => displayCocktail(data.drinks));
+const loadCocktails= async(searchText)=>{
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCocktail(data.drinks);
 }
 
 
-
-
 const displayCocktail = cocktail =>{
-    console.log(cocktail.slice(0,10));
-    const cocktailArr = cocktail.slice(10);
-    cocktailArr.forEach(cocktail=>{
-        
-        const cardSectionEl = document.getElementById('card-section');
+    const cardSectionEl = document.getElementById('card-section');
+    cardSectionEl.textContent ='';
+    const noCocktail = document.getElementById('no-msg-found');
+    cocktail = cocktail.slice(0,9);
+    if(cocktail.length ==='9'){
+        noCocktail.classList.remove('hidden'); 
+    }else{
+        noCocktail.classList.add('hidden')
+    }
+   
+    
+    cocktail.forEach(cocktail=>{
         const cardDiv = document.createElement('div');
-        cardDiv.classList.add('card' ,'card-compact' ,'w-96','bg-base-100' ,'shadow-xl','mx-auto','my-3')
-        cardDiv.innerHTML=`
-        
+        cardDiv.classList.add('card' ,'card-compact' ,'w-96','bg-base-100' ,'shadow-xl','mx-auto','my-3');
+        cardDiv.innerHTML=` 
         <figure><img src="${cocktail.strDrinkThumb}" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">${cocktail.strDrink}</h2>
@@ -33,12 +37,14 @@ const displayCocktail = cocktail =>{
     }
        )
 }
+    document.getElementById('search-cocktail').addEventListener('click', ()=>{
+    //  searchText = '';
+    const searchFieldEl = document.getElementById('search-field');
+    let searchText = searchFieldEl.value ;  
 
-const search = document.getElementById('search-cocktail').addEventListener('click', ()=>{
-    const searchCocktailEl = document.getElementById('search-cocktail');
-    console.log('hello');
-    const searchText = searchCocktailEl.value ;
-    return searchText;
+    loadCocktails(searchText);
+    
 })
 
-loadCocktails();
+
+// loadCocktails();
